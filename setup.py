c193031f764420a4adf20f6686898677d68be2a3
Smreeta Shrestha <91942225+Smreeta1@users.noreply.github.com>
2024-07-19 20:02:42 +0545

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
     name='pgsql_project', 
-    version='0.1.0',
+    version='1.0',
     description='A simple library management system using PostgreSQL',
-    author='Your Name', 
+    author='Smreeta', 
     packages=find_packages(), 
     install_requires=[
         'psycopg2-binary', 
